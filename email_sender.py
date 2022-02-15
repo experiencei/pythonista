a0eeaae6bc67c6dd3ced5d2652aec803a5b8016e
experiencei <ayelojahighbee01@gmail.com>
2022-02-15 15:30:09 +0100

    customizing the smtp to uses template

--- email_sender.py
+++ email_sender.py
@@ -1,12 +1,15 @@
 import smtplib 
 from email.message import EmailMessage
+from string import Template
+from pathlib import Path
 
+html = Template(Path('index.html').read_text())
 email = EmailMessage()
 email['froom'] = 'Ayeloja Ibrahim'
 email['to'] = '_ayelojahbd01@gmail.io'
 email['subject'] = 'You just got employed after passing your interview'
 
-email.set_content('Congratulations to you WOW!')
+email.set_content(html.substitute({'name': 'Ibrahim'}) , 'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
